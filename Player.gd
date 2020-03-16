622cd805ee29b59d3ed545fff6fd2988d368ca0a
unknown <rsmidt909@hotmail.com>
2020-03-16 17:37:28 -0500

    added in kinetic 2d character. Added in sprite. Added in action if ui_selet is pressed. Added in ability to move character. Action goes  by per frame per each space bar pressed

---
+++ Player.gd
@@ -0,0 +1,38 @@
+extends KinematicBody2D
+
+var MAX_SPEED = 500
+var ACCELERATION = 20000
+var motion = Vector2.ZERO
+
+func _process(_delta):
+	if Input.is_action_pressed("ui_select"):
+		$AnimatedSprite.play("LightningAttack")
+	else:
+		$AnimatedSprite.stop()
+
+func _physics_process(delta):
+	var axis = get_input_axis()
+	if axis == Vector2.ZERO:
+		apply_friction(ACCELERATION * delta)
+	else:
+		apply_movement(axis * ACCELERATION * delta)
+	motion = move_and_slide(motion)
+
+func get_input_axis():
+	var axis = Vector2.ZERO
+	axis.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	axis.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	return axis.normalized()
+	
+func apply_friction(amount):
+	if motion.length() > amount:
+		motion -= motion.normalized() * amount
+	else:
+			motion = Vector2.ZERO
+
+func apply_movement(acceleration):
+	motion =+ acceleration
+	if motion.length()>MAX_SPEED:
+		motion = motion.clamped(MAX_SPEED)
+
+
