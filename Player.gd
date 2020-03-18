fbf9f83417a1959e9eed67d20dd2ac8054f1571d
unknown <rsmidt909@hotmail.com>
2020-03-18 01:47:42 -0500

    created signals for emitting damage and ID. Created variable for ID for player.

--- Player.gd
+++ Player.gd
@@ -1,11 +1,15 @@
 extends KinematicBody2D
 
+
+
+var ID = "player"
 var MAX_SPEED = 500
 var ACCELERATION = 20000
 var motion = Vector2.ZERO
 var frameZero = 0
 var frameSeven = 8
 var frame
+signal sendDamage(amount, ID)
 
 #make attack bool for _process
 
@@ -18,12 +22,27 @@ var frame
 #gettargetID() x - targetIDHealth
 
 
+##################################################
+
+#Create one reciever signal - recieves in a format - object
+#create one emmiter signal - sends in a format - object
+#--------------------------------------------------
+# create attack and reciever does getbody func that sets bool if assigned playerID is in area. If so, and signal being emitted
+# cause damage
+#assign player ID
+
+#/# still need to make connection to node for signal recieving
+
 
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_select"):
+		#for for now, we damage thens how attack until design for while action.
+		emit_signal("sendDamage", 50, ID)
 		$AnimatedSprite.play("Attack")
 		if $AnimatedSprite.get_frame() == frameSeven:
 				$AnimatedSprite.set_frame(frameZero)
+
+
 
 
 func _physics_process(delta):
@@ -54,5 +73,6 @@ func apply_movement(acceleration):
 	if motion.length()>MAX_SPEED:
 		motion = motion.clamped(MAX_SPEED)
 
-
-
+func getColliderInfo():
+var target = $RayCast2D.get_collider()
+if target != null:
